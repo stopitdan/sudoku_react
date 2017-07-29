88a577d41b94beaa5375e1d164e4989191086641
Dan Wiegand <dan.wiegand@utrip.com>
2017-07-29 15:28:39 -0700

    commit 3.

--- src/client/app/sudoku.jsx
+++ src/client/app/sudoku.jsx
@@ -5,9 +5,40 @@ class Sudoku extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        grid : []
+        grid : [
+            [{},{},{}],
+            [{},{},{}],
+            [{},{},{}]
+        ]
     };
+  }
 
+  generateBoard(){
+      let board = this.state.grid;
+      let startingValues = [1,2,3];
+      let startingLength = startingValues.length
+      for(var i = 0; i < startingLength; i++){
+          let randomStart = randomStartingPosition();
+          let boardVal = board[randomStart[0]][randomStart[1]].value
+          if(!boardVal){
+              boardVal = startingValues[i];
+          } else {
+              i--
+          }
+      }
+  }
+
+  collision(){
+
+  }
+
+  randomStartingPosition() {
+      let array = [];
+      let index = Math.floor(Math.Random() * 3 + 1)
+      let position = Math.floor(Math.Random() * 3 + 1)
+
+      array.push(index, position);
+      return array;
   }
 
   render() {
